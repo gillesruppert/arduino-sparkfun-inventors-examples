e14a1ee46b9b9b9b04eac1ad43afaa75783a9691
Gilles Ruppert <gilles@madeofbytes.com>
2012-10-25 14:40:24 +0200

    clean up circ-01

--- circ-01.js
+++ circ-01.js
@@ -1,21 +1,20 @@
-var
-  five = require("johnny-five")
-, board = new five.Board()
-, ledPin = 13
-, OUTPUT = 1
+var five = require("johnny-five");
+var board = new five.Board();
+var ledPin = 13;
+var OUTPUT = 1;
 
 board.on("ready", function() {
-  this.pinMode(ledPin, OUTPUT)
-  var val = 0
+  this.pinMode(ledPin, OUTPUT);
+  var val = 0;
 
   this.loop(1000, function() {
-    this.digitalWrite(ledPin, toggle())
-  })
-})
+    this.digitalWrite(ledPin, toggle());
+  });
+});
 
 var toggle = (function() {
-  var val = 0
+  var val = 0;
   return function toggle() {
-    return (val = val ? 0 : 1)
+    return (val = val ? 0 : 1);
   }
-}())
+}());
